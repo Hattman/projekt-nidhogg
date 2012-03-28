5188dac41e68cc3202d67fe17887b3b08f7f2e1d
David Synnergren <a10davsy@student.his.se>
2012-03-28 19:41:27 +0200

    Lade till gameLoop som fungerar.

--- nidhogg/src/org/megabear/Nidhogg.java
+++ nidhogg/src/org/megabear/Nidhogg.java
@@ -1,5 +1,9 @@
 package org.megabear;
 
 public class Nidhogg {
+	
+	public void tick(){
+		//it moves
+	}
 
 }
--- nidhogg/src/org/megabear/Renderer.java
+++ nidhogg/src/org/megabear/Renderer.java
@@ -1,5 +1,15 @@
 package org.megabear;
 
 public class Renderer {
-
+	
+	Nidhogg m_game;
+	
+	public Renderer(Nidhogg p_game){
+		m_game = p_game;
+				
+	}
+	
+	public void draw(float p_interpolation){
+		//do magic
+	}
 }
