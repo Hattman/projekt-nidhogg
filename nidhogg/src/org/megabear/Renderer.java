67f65b220e3610242ee91c22c9a924f8cb878336
Tobias Lööw <a10toblo@student.his.se>
2012-04-11 19:45:54 +0200

    Lärt oss använda skiten lite mer

--- nidhogg/src/org/megabear/Renderer.java
+++ nidhogg/src/org/megabear/Renderer.java
@@ -1,15 +1,24 @@
 package org.megabear;
 
-public class Renderer {
-	
+import javax.swing.*;
+import java.awt.*;
+
+public class Renderer extends JFrame{
+	private static final long serialVersionUID = 1L;
 	Nidhogg m_game;
 	
 	public Renderer(Nidhogg p_game){
 		m_game = p_game;
-				
+		this.setSize(800,600);
+		this.setVisible(true);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
 	public void draw(float p_interpolation){
 		//do magic
+		Graphics g = getGraphics();
+		g.setColor(Color.GREEN);
+		g.fillRect(0, 560, 800, 40);
 	}
 }
